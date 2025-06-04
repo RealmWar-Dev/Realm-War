28ecbd4a8fcf31bc88de9a4b45abf17224f0f723
Mohamadamin Fahimi <mohamadaminfahimi85@gmail.com>
2025-06-04 17:00:20 +0330

    simple edit

--- Realm War/src/Launcher.java
+++ Realm War/src/Launcher.java
@@ -1,5 +1,6 @@
-public class Main {
+public class Launcher {
     public static void main(String[] args) {
 
     }
+
 }
--- Realm War/src/controller/TurnManager.java
+++ Realm War/src/controller/TurnManager.java
@@ -1,4 +1,7 @@
 package controller;
 
 public class TurnManager {
+    public static void main(String[] args) {
+
+    }
 }
